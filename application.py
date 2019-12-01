7c2232bd6e1d5e26ff030bdee4530a360e1656e7
Kunaal Ahuja <kunaalahuja@users.noreply.github.com>
2019-11-30 20:24:00 -0500

    Updated the data script, added author and genre

--- application.py
+++ application.py
@@ -9,14 +9,15 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-
+##############################################################
+##
+## Data Preparation
+##
+##############################################################
+                    
+#reviews_filename = "users_and_their_ratings_sam.csv"
 reviews_filename = "user_reviews_1.csv"
-
-##############################################################
-##
-## Collaborative Filtering
-##
-##############################################################
+books_filename = "books_with_details_Kunaal.csv"
 
 # Reading data from the CSV
 def get_data():
@@ -36,25 +37,19 @@ def data_cleanup(ratings_data_raw):
   #remove unnecessary columns
   # del ratings_data['Unnamed: 0']
   del ratings_data['Book_name']
+  #del ratings_data['average_rating']
   del ratings_data['Image_url']
 
   #rename columns
   ratings_data.rename(columns = {'Rating':'rating', 'User_id':'user_id', 'Book_id':'book_id'}, inplace = True) 
   return ratings_data
 
-#creates a map of book_id to image_url
-def get_book_url_map():
-  reviews_raw = pd.read_csv(reviews_filename)
-  book_url_df = reviews_raw[['Book_id', 'Image_url']]
-  book_url_df.rename(columns = {'Image_url':'image_url', 'Book_id':'book_id'}, inplace = True) 
-  return book_url_df.set_index('book_id').T.to_dict()
 
-#creates a map of book_id to book_name
-def get_book_name_map():
-  reviews_raw = pd.read_csv(reviews_filename)
-  book_url_df = reviews_raw[['Book_id', 'Book_name']]
-  book_url_df.rename(columns = {'Book_name':'book_name', 'Book_id':'book_id'}, inplace = True) 
-  return book_url_df.set_index('book_id').T.to_dict()
+##############################################################
+##
+## Collaborative Filtering
+##
+##############################################################
 
 #Build the tuples of ratings, book_id and user_id
 def buildMatrix(df):
@@ -133,6 +128,26 @@ def get_recommended_books(current_user_id, reInitialize = False, topN = 5, topBo
   similar_users = get_similar_users(ratings_matrix, user_id_index, current_user_id, topN)
   return recommend_book_from_sim_user(ratings_matrix, similar_users, user_id_index, index_book_id, topBooks)
 
+##############################################################
+##
+## Helper Functions
+##
+##############################################################
+
+#creates a map of book_id to image_url
+def get_book_url_map():
+  reviews_raw = pd.read_csv(reviews_filename)
+  book_url_df = reviews_raw[['Book_id', 'Image_url']]
+  book_url_df.rename(columns = {'Image_url':'image_url', 'Book_id':'book_id'}, inplace = True) 
+  return book_url_df.set_index('book_id').T.to_dict()
+
+#creates a map of book_id to book_name
+def get_book_name_map():
+  reviews_raw = pd.read_csv(reviews_filename)
+  book_url_df = reviews_raw[['Book_id', 'Book_name']]
+  book_url_df.rename(columns = {'Book_name':'book_name', 'Book_id':'book_id'}, inplace = True) 
+  return book_url_df.set_index('book_id').T.to_dict()
+
 #returns the avarage rating of a book
 def get_average_book_rating():
   reviews_raw = pd.read_csv(reviews_filename)
@@ -142,12 +157,12 @@ def get_average_book_rating():
 
 #Get n random books to show a new user 
 def get_random_books(n=10): 
-  reviews_raw = pd.read_csv(reviews_filename)
-  #add author and genre
-  book_df = reviews_raw[['Book_id', 'Book_name', 'Image_url']]
-  book_df.rename(columns = {'Book_name':'book_name_long', 'Book_id':'book_id', 'Image_url':'image_url'}, inplace = True) 
+  books_raw = pd.read_csv(books_filename)
+  book_df = books_raw[['book_id','author', 'book_url', 'book_name', 'genre', 'image_url', 'average_rating']]
+  book_df.rename(columns = {'book_name':'book_name_long'}, inplace = True) 
   book_df['book_id_copy'] = book_df['book_id']
   book_df['book_name'] = book_df['book_name_long'].astype(str).str[:30]
+  book_df['author'] = book_df['author'].astype(str).str[:30]
         
   book_df = book_df.sample(n=n)
   return book_df.set_index('book_id_copy').T.to_dict()
@@ -177,7 +192,14 @@ def get_custom_userID(random_books, rating1, rating2, rating3, rating4, rating5,
   cache_matrix(reInitialize = True, fetchDataFromCsv = False)
 
   return new_user_id
-  
+
+#gets all the information we've for each book  
+def get_book_info_map():
+  books_raw = pd.read_csv(books_filename)
+  book_info_df = books_raw[['book_id', 'author', 'book_url', 'book_name', 'genre', 'image_url', 'average_rating']]
+  #book_info_df["image_url"] = book_info_df["book_url"].str.replace("/show/", "/photo/")
+  return book_info_df.set_index('book_id').T.to_dict()
+
 
 #############################################################
 ##
@@ -207,7 +229,7 @@ def homepage():
 
     UserId   = request.form.get('UserId', default_user_id)
   
-    rating1 = rating2 = rating3 = rating4 = rating5 = rating6 = rating7 = rating8 = rating9 = rating10 = 0
+    rating1  = rating2 = rating3 = rating4 = rating5 = rating6 = rating7 = rating8 = rating9 = rating10 = 0
     rating1  = request.form.get('Rating1',  0)
     rating2  = request.form.get('Rating2',  0)
     rating3  = request.form.get('Rating3',  0)
@@ -229,8 +251,8 @@ def homepage():
     print("new userID", UserId)
     books = get_recommended_books(int(UserId)) 
     print("Userid: ",UserId, " -- ", books )
-    url_map = get_book_url_map()
-    name_map = get_book_name_map()
+
+    books_info_map = get_book_info_map()
 
     books = [ [book] for book in books]
     df = pd.DataFrame(books,  columns = ['BookID'])
@@ -241,21 +263,37 @@ def homepage():
     for row in df.values:
         userID = str(UserId)
         bookID = str(row[0])
-        book_name = name_map[row[0]]['book_name'][:30]
-        image_url = url_map[row[0]]['image_url']
-        
+
+        ##Get Book information for displaying
+        book_name  = books_info_map[row[0]]['book_name'][:30]
+        image_url  = books_info_map[row[0]]['image_url']
+        author     = books_info_map[row[0]]['author'][:30]
+        genre      = books_info_map[row[0]]['genre']
+        avg_rating = books_info_map[row[0]]['average_rating']
+        book_url   = books_info_map[row[0]]['book_url']
+
         # make a list of keys
         keys_list = []
         for item in d['children']:
             keys_list.append(item['userID'])
 
+        books_info = {
+            "bookID"    : bookID,
+            "book_name" : book_name,
+            "book_url"  : book_url,
+            "image_url" : image_url,
+            "author"    : author,
+            "genre"     : genre,
+            "avg_rating": avg_rating
+        }    
+
         # if 'the_parent' is NOT a key in the flare.json yet, append it
         if not userID in keys_list:
-            d['children'].append({"userID": userID, "children": [{"bookID": bookID, "book_name": book_name, "image_url": image_url}]})
+            d['children'].append({"userID": userID, "children": [books_info]})
 
         # if 'the_parent' IS a key in the flare.json, add a new child to it
         else:
-            d['children'][keys_list.index(userID)]['children'].append({"bookID": bookID, "book_name": book_name, "image_url": image_url})
+            d['children'][keys_list.index(userID)]['children'].append(books_info)
 
     flare = d
     e = json.dumps(flare)
