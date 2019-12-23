ee96a33bcf3a86d431e96a60294bbd3e9ac3aae0
Kunaal Ahuja <kunaalahuja@users.noreply.github.com>
2019-12-23 06:37:28 +0530

    add function to write all the csv

--- codebase/data_fetch.py
+++ codebase/data_fetch.py
@@ -10,13 +10,20 @@ import sys
 #GLOBAL COUNTERS
 
 TOTAL_USERS = 103525406
-SAMPLE_USERS = 100000
-USERS_COUNT = 12000
-#SAMPLE_USERS = 100
-#USERS_COUNT = 5
+#SAMPLE_USERS = 100000
+#USERS_COUNT = 12000
+SAMPLE_USERS = 100
+USERS_COUNT = 5
 
 #FILE NAMES
-USER_FILE = 'users.csv'
+USERS_FILE        = 'users.csv'
+REVIEWS_FILE      = 'reviews.csv'
+BOOKS_FILE        = 'books.csv'
+DATA_FILE         = 'master_data.csv'
+BOOKS_DETAIL_FILE = 'books_details.csv'
+BOOKS_DESC_FILE   = 'books_desc.csv'
+USER_RATINGS_FILE = 'user_ratings.csv'
+
 
 def get_goodreads_api_key():
     try:
@@ -109,9 +116,42 @@ def write_user_file():
     #TODO: merge the add username logic to get_user_data <Kunaal>
     user_df = add_username(user_df, key)
 
-    write_to_csv(user_df, USER_FILE)
+    write_to_csv(user_df, USERS_FILE)
 
+
+def create_csv_files():
+    #REVIEWS
+    reviews_df = pd.read_csv(REVIEWS_FILE)
+    reviews_df['user_id'] = reviews_df['user_id'].apply(int)
+    reviews_df.drop(columns = ['review description', 'author'], inplace=True)
+
+    books_df = pd.read_csv(BOOKS_FILE, encoding='iso-8859-1')
+    users_df = pd.read_csv(USERS_FILE, encoding='iso-8859-1')
+
+    books_data_df = pd.merge(reviews_df, books_df, how='left', on=['book_id'])
+    # Master data including all fields that we care about
+    master_df = pd.merge(books_data_df, users_df, how='left', on=['user_id'])
+    
+    write_to_csv(master_df, DATA_FILE)
+        
+    # Creating subsets of data relevant to different team members
+    book_details_df = books_data_df[['book_id','book_name','image_url','genre','book_url','average_rating','author']]
+    book_details_df.drop_duplicates(inplace=True)
+    write_to_csv(book_details_df, BOOKS_DETAIL_FILE)
+
+    book_desc_df = books_data_df[['book_description','book_id','book_name','average_rating']]
+    book_desc_df.drop_duplicates(inplace=True)
+    write_to_csv(book_desc_df, BOOK_DESC_FILE)
+    
+    user_ratings_df = books_data_df[['book_id','book_name','rating','user_id','average_rating']]
+    user_ratings_df.drop_duplicates(inplace=True)
+    write_to_csv(user_ratings_df, USER_RATINGS_FILE)
+    
 if __name__ == "__main__":
     operation = str(sys.argv[1])
     if operation == "users":
         write_user_file()
+    elif operation == "csv":
+        create_csv_files()
+    else:
+        print("[USAGE ERROR]: Choose one of the following operations: users, csv")    
